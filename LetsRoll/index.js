8b70c49d9290ff06d2cb21e63c9bc97885fcb6c6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-27 10:46:17 +0530

    Added a checkWin function to determine the winner and sets the heading accordingly

--- LetsRoll/index.js
+++ LetsRoll/index.js
@@ -6,7 +6,20 @@ const dice2 = document.getElementById("dice2")
 
 
 
-
+const checkWin = (r1, r2) => {
+    if (r1 > r2) {
+        console.log("Player 1 Won")
+        Heading.innerText = "Player1 Won"
+    }
+    else if (r1 < r2) {
+        console.log("Player 2 Won")
+        Heading.innerText = "Player2 Won"
+    }
+    else {
+        console.log("No One Won")
+        Heading.innerText = "Draw"
+    }
+}
 
 const randomNumber = () => {
     const random_number1 = Math.floor(Math.random() * 6) + 1;
@@ -16,6 +29,7 @@ const randomNumber = () => {
     dice1.setAttribute("src", dice1_image);
     dice2.setAttribute("src", dice2_image);
     console.log(random_number1, random_number2);
+    checkWin(random_number1, random_number2);
 }
 
 const Roll = () => {
