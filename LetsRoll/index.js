3d1af7ede8dc8d9695a8f2a8112462a20ff2cf68
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-27 10:42:25 +0530

    created variables and added click listener for the button

---
+++ LetsRoll/index.js
@@ -0,0 +1,14 @@
+
+const rollBtn = document.querySelector(".btn")
+const Heading = document.querySelector(".heading")
+const dice1 = document.getElementById("dice1")
+const dice2 = document.getElementById("dice2")
+
+
+const Roll = () => {
+    console.log(dice1, dice2, player1, player2);
+}
+
+
+
+rollBtn.addEventListener("click", Roll)
