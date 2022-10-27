d61be21901ed7f61283be70c36782677826167c3
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-27 10:44:08 +0530

    Created a random number generator function which sets dice images

--- LetsRoll/index.js
+++ LetsRoll/index.js
@@ -5,8 +5,22 @@ const dice1 = document.getElementById("dice1")
 const dice2 = document.getElementById("dice2")
 
 
+
+
+
+const randomNumber = () => {
+    const random_number1 = Math.floor(Math.random() * 6) + 1;
+    const random_number2 = Math.floor(Math.random() * 6) + 1;
+    const dice1_image = `assets/dice${random_number1}.png`;
+    const dice2_image = `assets/dice${random_number2}.png`;
+    dice1.setAttribute("src", dice1_image);
+    dice2.setAttribute("src", dice2_image);
+    console.log(random_number1, random_number2);
+}
+
 const Roll = () => {
     console.log(dice1, dice2, player1, player2);
+    randomNumber();
 }
 
 
