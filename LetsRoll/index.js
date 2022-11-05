43cedd07d8944db703cedc9c10b6ad65b0d679ea
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-11-05 13:02:23 +0530

    Added player scores and attempts functionallity

--- LetsRoll/index.js
+++ LetsRoll/index.js
@@ -3,26 +3,43 @@ const rollBtn = document.querySelector(".btn")
 const Heading = document.querySelector(".heading")
 const dice1 = document.getElementById("dice1")
 const dice2 = document.getElementById("dice2")
+const player1_score = document.getElementById("p1-score")
+const player2_score = document.getElementById("p2-score")
+const attempts = document.getElementById("attempts")
 
 
-// Checking the winner
+
+
+// Checking the winner and updating the Scores
 const checkWin = (r1, r2) => {
-    Heading.innerText = r1 > r2 ? "Player 1 Won" : r1 < r2 ? "Player 2 Won" : "Draw";
+    if (r1 === r2) Heading.innerText = "Draw";
+    else if (r1 > r2) {
+        player1_score.innerText = parseInt(player1_score.innerText) + 1;
+        Heading.innerText = "Player 1 Won";
+    }
+    else {
+        player2_score.innerText = parseInt(player2_score.innerText) + 1;
+        Heading.innerText = "Player 2 Won";
+    }
 }
+
 
 // Assigning random dice image to both dices
 const randomNumber = () => {
     const random_number1 = Math.floor(Math.random() * 6) + 1;
     const random_number2 = Math.floor(Math.random() * 6) + 1;
-    const dice1_image = `assets/dice${random_number1}.png`;
-    const dice2_image = `assets/dice${random_number2}.png`;
-    dice1.setAttribute("src", dice1_image);
+    const dice1_image = `assets/dice_${random_number1}.png`;
+    const dice2_image = `assets/dice_${random_number2}.png`;
     dice2.setAttribute("src", dice2_image);
-    checkWin(random_number1, random_number2);
+    dice1.setAttribute("src", dice1_image);
+    checkWin(random_number1,random_number2)
 }
 
 // Roll
 const Roll = () => {
+
+    attempts.innerText = parseInt(attempts.innerText) + 1;
+
     randomNumber();
 }
 
